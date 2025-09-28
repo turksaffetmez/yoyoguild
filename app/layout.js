0f82afdc5abbf9472f069873bbbc1323f33b2af6
Heisenberg YoYo <e.erbakili89@gmail.com>
2025-09-29 00:59:41 +0300

    YoYo Guild Battle V1

--- app/layout.js
+++ app/layout.js
@@ -58,17 +58,6 @@ export default function RootLayout({ children }) {
         <meta name="twitter:image" content="https://yoyoguild.vercel.app/images/page.png" />
         <meta name="twitter:site" content="@yoyoguild" />
         
-        {/* Farcaster Frame Tags */}
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://yoyoguild.vercel.app/images/page.png" />
-        <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-        <meta property="fc:frame:button:1" content="🎮 Play Game" />
-        <meta property="fc:frame:button:1:action" content="post" />
-        <meta property="fc:frame:button:1:target" content="https://yoyoguild.vercel.app/api/frame" />
-        <meta property="fc:frame:button:2" content="🏆 Leaderboard" /> 
-        <meta property="fc:frame:button:2:action" content="post" />
-        <meta property="fc:frame:button:2:target" content="https://yoyoguild.vercel.app/api/frame?action=leaderboard" />
-        
         {/* Farcaster Mini App Tags */}
         <meta property="fc:mini-app:name" content="YoYo Guild Battle" />
         <meta property="fc:mini-app:icon" content="https://yoyoguild.vercel.app/images/yoyo.png" />
