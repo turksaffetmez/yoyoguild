74ac4e04c156b7439dc248695b27b394d31023f4
Heisenberg YoYo <e.erbakili89@gmail.com>
2025-09-30 20:17:15 +0300

    YoYo Guild Battle V1

--- app/api/webhook/route.js
+++
@@ -1,8 +0,0 @@
-export async function POST(request) {
-  return new Response(JSON.stringify({
-    status: "ok",
-    message: "Webhook received"
-  }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
