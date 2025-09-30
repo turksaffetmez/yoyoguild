4db1a43587c8201224bc3ecbe7f030375959730e
Heisenberg YoYo <e.erbakili89@gmail.com>
2025-09-30 23:48:15 +0300

    YoYo Guild Battle V1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  trailingSlash: true,
   images: {
     unoptimized: true
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.farcaster.xyz https://*.farpcast.xyz https://farcaster.xyz https://warpcast.com https://*.base.dev https://base.dev https://www.base.dev;"
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          }
+        ],
+      },
+    ]
   },
 }
 
