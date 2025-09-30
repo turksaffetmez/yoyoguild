ed69311359d208c60d2d49096973efb2e5e1a401
Heisenberg YoYo <e.erbakili89@gmail.com>
2025-09-30 23:18:21 +0300

    YoYo Guild Battle V1

--- app/.well-known/farcaster.json/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.redirect(
-    'https://api.farcaster.xyz/miniapps/hosted-manifest/01999bd8-f007-9349-745c-f2da399795ef',
-    307
-  )
-}
